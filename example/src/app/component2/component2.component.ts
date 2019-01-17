413383a3010b1661a5b6cb7b2431127eaabdb068
Chris Lane <chris.lane359@gmail.com>
2019-01-16 20:59:57 -0800

    updating content

--- example/src/app/component2/component2.component.ts
+++ example/src/app/component2/component2.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-component2',
   templateUrl: './component2.component.html',
   styleUrls: ['./component2.component.css']
 })
-export class Component2Component {
+export class Component2Component implements OnInit {
+
+  constructor(private titleService: Title) {}
+  componentTitle = 'Title of Component 2';
+  setTitle(newTitle:string) { this.titleService.setTitle(newTitle); }
+
+  ngOnInit() {
+    this.setTitle(this.componentTitle);
+  }
+
 }
+
--- example/src/app/component3/component3.component.ts
+++ example/src/app/component3/component3.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-component3',
   templateUrl: './component3.component.html',
   styleUrls: ['./component3.component.css']
 })
-export class Component3Component {
+export class Component3Component implements OnInit {
+
+  constructor(private titleService: Title) {}
+  componentTitle = 'Title of Component 3';
+  setTitle(newTitle:string) { this.titleService.setTitle(newTitle); }
+
+  ngOnInit() {
+    this.setTitle(this.componentTitle);
+  }
+
 }
+
